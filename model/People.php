df017c7c8fd7d29495727fdd4d557c21b20387cb
Brenno Hoffmann <brenno_hoffmann@outlook.com>
2019-06-03 12:29:51 -0300

    Criando mvc

---
+++ model/People.php
@@ -0,0 +1,41 @@
+<?php
+
+class People{
+    
+    private $name;
+    private $age;
+    private $cpf;
+    
+
+    public function getName(){
+        return  $this->name;
+    }
+
+    public function setName($name){
+        $this->name = $name;
+    }
+
+    public function getAge(){
+        return  $this->age;
+    }
+
+    public function setAge($age){
+        $this->age = $age;
+    }
+
+    public function getCpf(){
+        return  $this->cpf;
+    }
+
+    public function setCpf($cpf){
+        $this->cpf = $cpf;
+    }
+    
+
+}
+
+    // public function registerPeople($con){}
+
+
+
+?>
